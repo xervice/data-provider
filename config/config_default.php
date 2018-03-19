89ee4afcc4221e9c6347e0805295a815cf152bf7
mibexx <contact@mibexx.de>
2018-03-19 19:43:37 +0100

    * Schema fix

--- config/config_default.php
+++ config/config_default.php
@@ -3,7 +3,7 @@
 use Xervice\DataProvider\DataProviderConfig;
 
 $config[DataProviderConfig::DATA_PROVIDER_PATHS] = [
-    __DIR__ . '/../src/*/Schema/'
+    __DIR__ . '/../src/Xervice/*/Schema/'
 ];
 
 $config[DataProviderConfig::DATA_PROVIDER_GENERATED_PATH] = __DIR__ . '/../src/Generated';
